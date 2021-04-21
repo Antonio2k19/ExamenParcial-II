d9fe5da7dd694f94c147e02b7672b15d349c80e9
Antonio <56178282+Antonio2k19@users.noreply.github.com>
2021-04-20 22:36:52 -0600

    Modelos pedidos + producto
    Modelo Pedidos + Modelo Producto

--- ExamenParcial2TemarioA/src/java/org/modelos/Pedido.java
+++ ExamenParcial2TemarioA/src/java/org/modelos/Pedido.java
@@ -1,11 +1,82 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.modelos;
 
 
 public class Pedido {
+    private int idPedido;
+    private int idCliente;
+    private String Observacion;
+    private int idDepartamento;
+    private int idMunicipio;
+    private String Estado;
+    private String Total;
+    private String TotalUsd;
+
+    public Pedido() {
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getObservacion() {
+        return Observacion;
+    }
+
+    public void setObservacion(String Observacion) {
+        this.Observacion = Observacion;
+    }
+
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public int getIdMunicipio() {
+        return idMunicipio;
+    }
+
+    public void setIdMunicipio(int idMunicipio) {
+        this.idMunicipio = idMunicipio;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+
+    public String getTotal() {
+        return Total;
+    }
+
+    public void setTotal(String Total) {
+        this.Total = Total;
+    }
+
+    public String getTotalUsd() {
+        return TotalUsd;
+    }
+
+    public void setTotalUsd(String TotalUsd) {
+        this.TotalUsd = TotalUsd;
+    }
     
 }
--- ExamenParcial2TemarioA/src/java/org/modelos/Producto.java
+++ ExamenParcial2TemarioA/src/java/org/modelos/Producto.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package org.modelos;
 
-/**
- *
- * @author javie
- */
+
 public class Producto {
+    private int idProducto;
+    private String Descripcion;
+    private String Precio;
+    private String Existencia;
+    private String Estado;
+
+    public Producto() {
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public String getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(String Precio) {
+        this.Precio = Precio;
+    }
+
+    public String getExistencia() {
+        return Existencia;
+    }
+
+    public void setExistencia(String Existencia) {
+        this.Existencia = Existencia;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
     
 }
